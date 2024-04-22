0f84d2e00d4857dc5ad9660a97fccb39c62b1918
Lucas Felippe <lukzfelippe@gmail.com>
2024-04-22 19:54:58 -0400

    criando opções

---
+++ app.py
@@ -0,0 +1,40 @@
+import os
+
+exibir_nome_do_programa():
+print("""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
+""")
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurantes')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = int(input('Escolha uma opção: '))
+
+def finalizar_app():
+    os.system('cls')
+    print('Finalizando o app\n')
+
+    
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('Listar Restaurantes')
+elif opcao_escolhida == 3:
+    print('Ativar Restaurante')
+else:
+    finalizar_app()
+
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
